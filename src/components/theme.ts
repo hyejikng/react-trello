03ca0727d71dabf316092f96fd0e6176cf8bc9f9
hyejikng <hyeji8842@gmail.com>
2023-11-06 23:53:42 +0900

    set files

---
+++ src/components/theme.ts
@@ -0,0 +1,22 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  bgColor: '#0a3d62',
+  textColor: '#d1ccc0',
+  accentColor: '#f6b93b',
+  listColor: '#fff',
+};
+
+export const LightTheme: DefaultTheme = {
+  bgColor: '#f1f2f6',
+  textColor: '#1e272e',
+  accentColor: '#ff6b81',
+  listColor: '#fff',
+};
+
+export const DarkTheme: DefaultTheme = {
+  bgColor: '#57606f',
+  textColor: '#dfe4ea',
+  accentColor: '#b71540',
+  listColor: '#3c6382',
+};
