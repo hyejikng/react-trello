4c05317d7a5f79f421fbcc3c62e1f8acb81250d2
hyejikng <hyeji8842@gmail.com>
2023-11-10 03:24:53 +0900

    Draggable Snapshot

--- src/components/DraggableCard.tsx
+++ src/components/DraggableCard.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
 import { styled } from 'styled-components';
 
-const Card = styled.div`
+const Card = styled.div<{ isDragging: boolean }>`
   border-radius: 5px;
   padding: 10px;
   margin-bottom: 6px;
-  background-color: ${(props) => props.theme.cardColor};
+  background-color: ${(props) =>
+    props.isDragging ? 'tomato' : props.theme.cardColor};
+  box-shadow: ${(props) =>
+    props.isDragging ? '0px 2px 5px rgba(0,0,0,0.1)' : 'none'};
 `;
 
 interface IDraggableCardProps {
@@ -20,8 +23,9 @@ function DraggableCard({ toDo, index }: IDraggableCardProps) {
   return (
     <div>
       <Draggable key={toDo} draggableId={toDo} index={index}>
-        {(magic) => (
+        {(magic, snapshot) => (
           <Card
+            isDragging={snapshot.isDragging}
             ref={magic.innerRef}
             {...magic.draggableProps}
             {...magic.dragHandleProps}
